891a5ec16d8cb7c813e67cd0bdbfce4686122e77
Kassim-gif <shuaibkassim44@gmail.com>
2023-08-13 12:00:08 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
+
 /**
-*main-A program that prints a line using the printf function.
-*Return:0(Success)
+* main - Fxn beiings, i'ts the
+*enrty poin for all c-programs
+*
+*
+* Return: returns zoro (int)
+*
 */
+
 int main(void)
 {
-print("with proper grammar,but the outcome is a piece of art,\n")
-return(0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+
+return (0);
 }
-~             
+
