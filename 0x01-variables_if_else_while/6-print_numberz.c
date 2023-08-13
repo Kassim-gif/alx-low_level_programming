d1ff7badeeab7ba5476febaaed7a5193ed62dfce
Kassim-gif <shuaibkassim44@gmail.com>
2023-08-13 12:21:32 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+/**
+ * main - prints the digits of the alphabets
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+/*Declarations*/
+	int digit;
+
+	for (digit = 48; digit < 58; digit++)
+	{
+	/*putchar(digit);*/
+	putchar(digit);
+
+	}
+	putchar('\n');
+	return (0);
+}
