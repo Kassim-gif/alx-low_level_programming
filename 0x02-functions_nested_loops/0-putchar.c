014c866607d3d56a44c298bc7972016959365e47
ShuaibKassim <126048278+Kassim-gif@users.noreply.github.com>
2023-08-17 04:11:48 +0300

    Update 0-putchar.c
    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - Prints _putchar followwed by \n
+ *
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	_putchar('_');
+  _putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	
+	return (0);
+}
