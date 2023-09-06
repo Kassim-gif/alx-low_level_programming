d462924650b69ce806b9ae6595f2d53f95c9aee4
ShuaibKassim <126048278+Kassim-gif@users.noreply.github.com>
2023-09-06 19:43:37 +0300

    2-strchr.c

--- 0x09-static_libraries/2-strchr.c
+++ 0x09-static_libraries/2-strchr.c
@@ -1,25 +1,18 @@
 #include "main.h"
-
 /**
- * _strchr - prints found c
- * @s: pointer to char
- * @c: char params to found
- * Return: *S
+ * _strchr - Entry point
+ * @s: input
+ * @c: input
+ * Return: Always 0 (Success)
  */
-
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
-	{
-		if (*s == c)
-		{
-			return (s);
-		}
-		s++;
-	}
-	if (*s == c)
-	{
-		return (s);
-	}
-	return (0);
+        	int i = 0;
+ 
+        	for (; s[i] >= '\0'; i++)
+        	{
+                    	if (s[i] == c)
+                                	return (&s[i]);
+        	}
+        	return (0);
 }
