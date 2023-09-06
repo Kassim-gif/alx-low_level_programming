58bb29cffcc30bd56c7b5954f2360ed8d3928ec7
ShuaibKassim <126048278+Kassim-gif@users.noreply.github.com>
2023-09-06 19:36:16 +0300

    0-memset.c

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -1,21 +1,62 @@
+
 #include "main.h"
 
+
+
+
 /**
- * _memset - memset func
- * @s: pointer var s
- * @b: value(s) to be chabed
- * @n: the index
+
+
+ * _memset - Program fills a block of memory with a specific value
+
+
+ * @s: starting address of memory to be filled
+
+
+ * @b: the desired value
+
+
+ * @n: number of bytes to be changed
+
+
  *
- * Return: returns *s
+
+
+ * Return: change array with new value for n bytes
+
+
  */
 
-char *_memset(char *s, char b, unsigned int n)
-{
-	unsigned int i;
 
-	for (i = 0; i < n; i++)
-	{
-		s[i] = b;
-	}
-	return (s);
+char *_memset(char *s, char b, unsigned int n)
+
+
+{
+
+
+   	int a = 0;
+
+
+ 
+
+
+   	for (; n > 0; a++)
+
+
+   	{
+
+
+          	s[a] = b;
+
+
+          	n--;
+
+
+   	}
+
+
+   	return (s);
+
+
 }
+
