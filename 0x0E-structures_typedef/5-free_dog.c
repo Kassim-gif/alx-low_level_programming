109f17dade112c60d80616c161ccd245bf240159
Kassim-gif <shuaibkassim44@gmail.com>
2023-09-12 12:39:49 -0700

    free

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees memory allocated  struct dog
+ * @d: struct dog to free
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
+
