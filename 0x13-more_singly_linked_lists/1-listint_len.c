200bf764a684e7155be9b6cd8dc7456030a249a3
kassim-gif <shuaibkassim44@gmail.com>
2023-09-25 07:53:27 -0700

    List length

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include <list.h>
+
+/**
+ * listint_len - rtrns  numb of elements in linked lists
+ * @h: linked list of type listint_t to traverse
+ *
+ * Return: number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+ size_t num = 0;
+
+ while (h)
+ {
+ num++;
+ h = h->next;
+ }
+
+ return (num);
+}
+
