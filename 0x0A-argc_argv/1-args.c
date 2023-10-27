f3c30591bceb445cd358f5d1e558d788005ee478
ShuaibKassim <126048278+Kassim-gif@users.noreply.github.com>
2023-10-27 23:45:54 +0300

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main - Entry poinear
+ *
+ *@argc: Number of comand line arguents
+ *@argv: An array containing str values of rthe command line arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	if (argc > 0)
+		printf("%d\n", argc - 1);
+	return (0);
+}
