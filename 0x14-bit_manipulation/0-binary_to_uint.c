d770a7fa262e877cba47003214348685f404fa54
Kassim-gif <shuaibkassim44@gmail.com>
2023-11-23 14:48:45 -0800

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -5,7 +5,7 @@
  * @b: string containing the binary number
  * Return: the converted number
  */
-unsigned int binary_to_uint(const char *b)
+unsigned int binary_to_uint(const char *b);
 {
 	int i;
 	unsigned int dec_val = 0;
